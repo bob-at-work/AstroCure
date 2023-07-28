7e42ff1f7cb430dc5a6b13a826c775071f6d9848
Bob's Cubicle <miraclemind.bobdev5@gmail.com>
2023-07-28 11:45:42 +0530

    Color Therapy Card flip

--- app/src/main/java/com/astrocure/ui/games/ColorTherapyActivity.java
+++ app/src/main/java/com/astrocure/ui/games/ColorTherapyActivity.java
@@ -1,19 +1,25 @@
 package com.astrocure.ui.games;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextPaint;
-import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.astrocure.R;
 import com.astrocure.databinding.ActivityColorTherapyBinding;
+import com.astrocure.databinding.DialogColorTherapyBinding;
+import com.astrocure.utils.AppConstantMethods;
 
 public class ColorTherapyActivity extends AppCompatActivity {
     ActivityColorTherapyBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +40,48 @@ public class ColorTherapyActivity extends AppCompatActivity {
                 }, null, Shader.TileMode.MIRROR);
         binding.title.getPaint().setShader(textShader);
 
+
+        binding.red.setOnClickListener(v -> {
+            setFlipCard(R.drawable.red,R.string.dummy_1);
+        });
+        binding.white.setOnClickListener(v -> {
+            setFlipCard(R.drawable.white,R.string.dummy_2);
+        });
+        binding.yellow.setOnClickListener(v -> {
+            setFlipCard(R.drawable.yellow,R.string.dummy_3);
+        });
+        binding.brown.setOnClickListener(v -> {
+            setFlipCard(R.drawable.brown,R.string.dummy_1);
+        });
+        binding.purple.setOnClickListener(v -> {
+            setFlipCard(R.drawable.purple,R.string.dummy_2);
+        });
+        binding.pink.setOnClickListener(v -> {
+            setFlipCard(R.drawable.pink,R.string.dummy_3);
+        });
+        binding.gray.setOnClickListener(v -> {
+            setFlipCard(R.drawable.grey,R.string.dummy_1);
+        });
+        binding.black.setOnClickListener(v -> {
+            setFlipCard(R.drawable.black,R.string.dummy_2);
+        });
+        binding.green.setOnClickListener(v -> {
+            setFlipCard(R.drawable.green,R.string.dummy_3);
+        });
+
+    }
+
+    private void setFlipCard(int drawable, int text) {
+        Dialog dialog = new Dialog(ColorTherapyActivity.this);
+        DialogColorTherapyBinding therapyBinding = DialogColorTherapyBinding.inflate(getLayoutInflater());
+        dialog.setContentView(therapyBinding.getRoot());
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(true);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
+        therapyBinding.textLayout.predictedText.setText(text);
+        therapyBinding.cardLayout.colorCard.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),drawable));
+        AppConstantMethods.flipCard(getApplicationContext(), therapyBinding.viewBack, therapyBinding.viewFront);
+        dialog.show();
     }
 }
--- app/src/main/java/com/astrocure/utils/AppConstantMethods.java
+++ app/src/main/java/com/astrocure/utils/AppConstantMethods.java
@@ -1,7 +1,15 @@
 package com.astrocure.utils;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.astrocure.R;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,6 +47,47 @@ public class AppConstantMethods {
             return null;
         }
         return json;
+    }
+
+    public static void flipCard(Context context, View visibleView, View inVisibleView) {
+        try {
+            visibleView.setVisibility(View.VISIBLE);
+            Float scale = context.getResources().getDisplayMetrics().density;
+            Float cameraDistance = 8000*scale;
+            visibleView.setCameraDistance(cameraDistance);
+            inVisibleView.setCameraDistance(cameraDistance);
+            AnimatorSet flipOutAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_out);
+            flipOutAnimatorSet.setTarget(inVisibleView);
+            AnimatorSet flipInAnimatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(context,R.animator.flip_in);
+            flipInAnimatorSet.setTarget(visibleView);
+            flipOutAnimatorSet.start();
+            flipInAnimatorSet.start();
+            flipInAnimatorSet.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(@NonNull Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(@NonNull Animator animation) {
+                    inVisibleView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(@NonNull Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(@NonNull Animator animation) {
+
+                }
+            });
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
